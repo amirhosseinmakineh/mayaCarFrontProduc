69ee645f63f475ba6d79db3931fcd2c2984c5326
amirhosseinmakineh <75474193+amirhosseinmakineh@users.noreply.github.com>
2026-09-18 18:20:42 +0330

    chore: use api.mayakhodro.com

--- src/app/services/auth/auth-service.ts
+++ src/app/services/auth/auth-service.ts
@@ -8,7 +8,7 @@ import { RegisterDto } from "../../models/user/registerDto";
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly baseUrl = 'https://mayakhoddrobackend-mayacar.runflare.run/api';
+  private readonly baseUrl = 'https://api.mayakhodro.com/api';
   //private readonly baseUrl = 'http://localhost:8080/api';
   private TOKEN_KEY = 'token';
   private http = inject(HttpClient);
